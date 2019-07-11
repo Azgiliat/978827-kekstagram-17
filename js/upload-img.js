46b0bf754a2e4108edb3b82532cbb838d9a687f3
Azgiliat <toxa2088@gmail.com>
2019-07-12 01:03:51 +0300

    bigger-smaller

--- js/upload-img.js
+++ js/upload-img.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const DEFAULT_SCALE = '100%';
 var fileImgInput = document.querySelector('.img-upload__form input[type=file]');
 var imgUpload = document.querySelector('.img-upload__overlay');
 var imgPreview = imgUpload.querySelector('.img-upload__preview img');
@@ -24,7 +25,6 @@ var removeFilters = function(photo) {
     photo.classList.remove('effects__preview--heat');
   }
 };
-
 var addFilter = function(photo, filter) {
   if (filter.classList.contains('effects__preview--none')) {
     photo.classList.add('effects__preview--none');
@@ -46,23 +46,44 @@ var addFilter = function(photo, filter) {
     photo.classList.add('effects__preview--heat');
   }
 };
+var canBeBigger = function(scale) {
+  return scale === '100%' ? 0 : 1;
+};
+var canBeSmaller = function(scale) {
+  return scale === '25%' ? 0 : 1;
+};
 
 fileImgInput.addEventListener('change', function() {
   var effectItems = imgUpload.querySelectorAll('.effects__item .effects__preview');
   var photoURL = window.URL.createObjectURL(this.files[0]);
   var effectsList = document.querySelector('.effects__list');
+  var scaleValue = imgUpload.querySelector('.scale__control--value');
+  var scaleControlBig = imgUpload.querySelector('.scale__control--bigger');
+  var scaleControlSmall = imgUpload.querySelector('.scale__control--smaller');
   imgUpload.classList.remove('hidden');
   imgPreview.src = photoURL;
 
   for (var i = 0; i < effectItems.length; i++) {
-    effectItems[i].style = 'background-image: url(' + photoURL + ')';
+    effectItems[i].style.backgroundImage = 'url(' + photoURL + ')';
   }
 
   effectsList.addEventListener('click', function(evt) {
     removeFilters(imgPreview);
     addFilter(imgPreview, evt.target);
-    console.log('evt target', evt.target);
-    console.log('evt currentTarget', evt.currentTarget);
     evt.preventDefault();
   }, false);
+
+  scaleValue.value = DEFAULT_SCALE;
+  scaleControlBig.addEventListener('click', function() {
+    if (canBeBigger(scaleValue.value)) {
+      imgPreview.style.transform = 'scale(' + (parseInt(scaleValue.value, 10) + 25)/100 + ')';
+      scaleValue.value = (parseInt(scaleValue.value, 10) + 25) + '%';
+    }
+  });
+  scaleControlSmall.addEventListener('click', function() {
+    if (canBeSmaller(scaleValue.value)) {
+      imgPreview.style.transform = 'scale(' + (parseInt(scaleValue.value, 10) - 25)/100 + ')';
+      scaleValue.value = (parseInt(scaleValue.value, 10) - 25) + '%';
+    }
+  });
 });
