2bf552809a7408de1fe27d5d06afe42d92dbab4c
Azgiliat <toxa2088@gmail.com>
2019-07-19 20:40:31 +0300

    hash

---
+++ js/checkValidity.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () = {
+  var hashtagInput = document.querySelector('.text__hashtags');
+  var submitButton = document.querySelector('.img-upload__submit');
+
+  submitButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+  });
+})();
