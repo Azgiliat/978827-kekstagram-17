ebe1fa35e57346a03e18bb22b279811fe5483b04
Azgiliat <toxa2088@gmail.com>
2019-08-01 01:20:21 +0300

    travis-fix

--- js/load-photos.js
+++ js/load-photos.js
@@ -1,3 +1,10 @@
+'use strict';
+
 (function () {
   var xhr = new XMLHttpRequest();
+  var URL = 'https://js.dump.academy/kekstagram/data';
+  window.donwloadedPictures = [];
+
+  xhr.open('GET', URL);
+  xhr.send();
 })();
