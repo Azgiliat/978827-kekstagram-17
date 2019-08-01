9d8102fb76d3380d4e6cb037fd9dd28810b4603f
Azgiliat <toxa2088@gmail.com>
2019-08-01 10:07:17 +0300

    load-photos-0.1

--- js/load-photos.js
+++ js/load-photos.js
@@ -5,6 +5,30 @@
   var URL = 'https://js.dump.academy/kekstagram/data';
   window.donwloadedPictures = [];
 
+  var onXhrLoad = function () {
+    var error;
+    switch (xhr.status) {
+      case 200:
+        window.donwloadedPictures = JSON.parse(xhr.responseText);
+        break;
+      case 400:
+        error = 'Неверный запрос';
+        break;
+      case 401:
+        error = 'Пользователь не авторизован';
+        break;
+      case 404:
+        error = 'Ничего не найдено';
+        break;
+      default:
+        error = 'Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText;
+    }
+    if (error) {
+      xhr.removeEventListener('load', onXhrLoad);
+    }
+    xhr.removeEventListener('load', onXhrLoad);
+  };
+  xhr.addEventListener('load', onXhrLoad);
   xhr.open('GET', URL);
   xhr.send();
 })();
